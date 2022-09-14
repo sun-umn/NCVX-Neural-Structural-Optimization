b35b4e736f607e5e01accd6deb1619933adf81df
Buyun-Liang <liang664@umn.edu>
2022-09-14 15:07:07 -0500

    clean main

--- unit_test/test_pygranso.py
+++ unit_test/test_pygranso.py
@@ -1,39 +1,21 @@
 import time
 import torch
 import sys
-
-# # from neural_structural_optimization.topo_physics import get_stiffness_matrix
-# ## Adding PyGRANSO directories. Should be modified by user
+# Adding current directories. Should be modified by user
 sys.path.append('/home/buyun/Documents/GitHub/NCVX-Experiments-PAMI')
 from pygranso.pygranso import pygranso
 from pygranso.pygransoStruct import pygransoStruct
 from pygranso.private.getNvar import getNvarTorch
-import torch.nn as nn
-import torch.nn.functional as Fun
-import torch
-import numpy as np
 
 from utils import models
 
 device = torch.device('cuda')
 double_precision = torch.double
-
-# def normalization(inputs, epsilon=1e-6):
-#   variance, mean = torch.var_mean(inputs)
-#   x = inputs
-#   x -= mean
-#   x *= torch.rsqrt(variance + epsilon)
-#   return x
-
-# def UpSampling(scale_factor):
-#   return Fun.upsample(scale_factor = scale_factor, mode='bilinear') # AD problem with bilinear
-
-
-
+torch.manual_seed(42)
 
 def user_fn(model,z, V0, K, F):
-    x = torch.squeeze(model(z),1)
-    U = list(model.parameters())[0] # shape: [60*20,1]
+    x = torch.squeeze(model(z),1) # DIP like strategy
+    U = list(model.parameters())[0] # dummy variable, shape: [60*20,1]
 
     # objective function
     f = U.T@K@U
@@ -47,12 +29,10 @@ def user_fn(model,z, V0, K, F):
     box_constr = torch.clamp(box_constr, min=0)
     folded_constr = torch.sum(box_constr**2)**0.5
     ci.c1 = folded_constr
-    # ci = None
 
     # equality constraint
     ce = pygransoStruct()
     ce.c1 = torch.mean(x) - V0
-
     ce.c2 = torch.sum((K@U - F)**2)**0.5 
 
     return [f,ci,ce]
@@ -65,30 +45,25 @@ z = torch.randn(1,128).to(device=device, dtype=double_precision) # initial fixed
 model = models.CNNModel_torch().to(device=device, dtype=double_precision)
 model.train()
 
-
-
 comb_fn = lambda model : user_fn(model,z, V0, K, F)
 
-
-
+# PyGRANSO options
 opts = pygransoStruct()
 opts.torch_device = device
 nvar = getNvarTorch(model.parameters())
 opts.x0 = torch.nn.utils.parameters_to_vector(model.parameters()).detach().reshape(nvar,1)
-# opts.opt_tol = 1e-3
-# opts.viol_eq_tol = 1e-4
-# opts.maxit = 150
-# opts.fvalquit = 1e-6
-# opts.print_level = 1
-# opts.print_frequency = 50
-# opts.print_ascii = True
 opts.limited_mem_size = 20
 opts.double_precision = True
+opts.mu0 = 1
+opts.maxit = 200
 
-# opts.mu0 = 1
+# torch.autograd.set_detect_anomaly(True) # DEBUG option
 
-torch.autograd.set_detect_anomaly(True)
+# # DEBUG part: print pygranso optimization variables
+# for name, param in model.named_parameters():
+#     print("{}: {}".format(name, param.data.shape))
 
+# main function call
 start = time.time()
 soln = pygranso(var_spec= model, combined_fn = comb_fn, user_opts = opts)
 end = time.time()
@@ -99,16 +74,3 @@ print("Total Wall Time: {}s".format(end - start))
 
 
 
-
-
-
-
-
-
-# # DEBUG part:
-# output = model(z)
-# # [1,20,60] [batch_size,width,height]
-# output = torch.squeeze(output,1)
-# # print(list(model.parameters())[0].shape)
-# for name, param in model.named_parameters():
-#     print("{}: {}".format(name, param.data.shape))
