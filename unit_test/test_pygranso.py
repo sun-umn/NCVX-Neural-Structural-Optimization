4422436df8743e32e37ba912c01f1faa29987e3d
Buyun-Liang <liang664@umn.edu>
2022-09-14 14:56:58 -0500

    clean model

--- unit_test/test_pygranso.py
+++ unit_test/test_pygranso.py
@@ -13,10 +13,7 @@ import torch.nn.functional as Fun
 import torch
 import numpy as np
 
-# from utils import models
-# from utils import problems
-# from utils import topo_api
-# from utils import train
+from utils import models
 
 device = torch.device('cuda')
 double_precision = torch.double
@@ -31,96 +28,7 @@ double_precision = torch.double
 # def UpSampling(scale_factor):
 #   return Fun.upsample(scale_factor = scale_factor, mode='bilinear') # AD problem with bilinear
 
-class CNNModel_torch(nn.Module):
 
-  def __init__(
-      self,
-    #   seed=0,
-      args=None,
-      latent_size=128,
-      dense_channels=32,
-    #   resizes=(1, 2, 2, 2, 1),
-      conv_filters=(128, 64, 32, 16, 1),
-    #   offset_scale=10,
-      kernel_size=(5, 5),
-    #   latent_scale=1.0,
-      dense_init_scale=1.0,
-    #   activation=nn.Tanh,
-    #   conv_initializer=tf.initializers.VarianceScaling,
-    #   normalization=global_normalization,
-  ):
-    # super().__init__(seed, args)
-    super().__init__()
-    resizes=(1, 1, 2, 2, 1)
-
-    if len(resizes) != len(conv_filters):
-      raise ValueError('resizes and filters must be same size')
-
-    # self.activation = nn.Tanh
-
-    total_resize = int(np.prod(resizes))
-    self.h = h = 20 // total_resize
-    self.w = w = 60 // total_resize
-    self.dense_channels = dense_channels
-    self.resizes = resizes
-    self.conv_filters = conv_filters
-    self.kernel_size = kernel_size
-
-    # net = inputs = layers.Input((latent_size,), batch_size=1)
-    inputs = torch.randn(1,latent_size)
-
-    filters = dense_channels * h * w 
-
-    nn.init.orthogonal_(inputs,gain=dense_init_scale * np.sqrt(max(filters / latent_size, 1)))
-
-    # dense_initializer = tf.initializers.orthogonal(
-    #     dense_init_scale * np.sqrt(max(filters / latent_size, 1)))
-    # net = layers.Dense(filters, kernel_initializer=dense_initializer)(net)
-    self.dense = nn.Linear(latent_size,filters)
-    # self.activation = nn.Tanh()
-    self.conv = nn.ModuleList()
-    self.conv_bn = nn.ModuleList()
-
-    for in_channels, out_channels in zip((dense_channels, 128, 64, 32, 16), conv_filters):
-      self.conv.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=self.kernel_size, padding='same'))
-      self.conv_bn.append(nn.BatchNorm2d(num_features=in_channels))
-
-    
-    # not used in the CNN model. It's a dummy variable used in PyGRANSO that has to be defined there, 
-    # as PyGRANSO will read all parameters from the nn.parameters() 
-    self.U =torch.nn.Parameter(torch.randn(60*20,1))
-
-    # outputs = tf.squeeze(net, axis=[-1])
-
-    # self.core_model = tf.keras.Model(inputs=inputs, outputs=outputs)
-
-    # latent_initializer = tf.initializers.RandomNormal(stddev=latent_scale)
-    # self.z = self.add_weight(
-    #     shape=inputs.shape, initializer=latent_initializer, name='z')
-
-
-  def forward(self,x):
-    x = self.dense(x)
-    x = x.reshape((1,self.dense_channels,self.h, self.w))
-
-    for resize, filters, i in zip(self.resizes, self.conv_filters,list(range(len(self.conv_filters)))):
-      x = Fun.tanh(x)
-      # print(x.shape)
-      x = Fun.upsample(x,scale_factor = resize, mode='bilinear')
-      # print(x.shape)
-      x = self.conv_bn[i](x)
-      x = self.conv[i](x)
-      # print(x.shape)
-      
-    return x
-
-      # TODO: weight intilizer for the convolution kernel
-      # TODO: AddOffSet
-      # TODO: squeeze output: [1,20,60,1] -> [1,20,60]  
-      # TODO: latent initializer
-
-      # if offset_scale != 0:
-      #   net = AddOffset(offset_scale)(net)
 
 
 def user_fn(model,z, V0, K, F):
@@ -154,7 +62,7 @@ K = torch.randn((60*20,60*20)).to(device=device, dtype=double_precision) # Stiff
 F = torch.randn((60*20,1)).to(device=device, dtype=double_precision) # Force vector
 z = torch.randn(1,128).to(device=device, dtype=double_precision) # initial fixed random input for DIP; similar to random seeds
 
-model = CNNModel_torch().to(device=device, dtype=double_precision)
+model = models.CNNModel_torch().to(device=device, dtype=double_precision)
 model.train()
 
 
