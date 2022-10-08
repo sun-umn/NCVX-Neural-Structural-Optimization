dd05f855347c8af3c09381c487f184c51d67352d
Ryan <ryan@tesorio.com>
2022-10-08 14:16:26 -0700

    adding the experiments.py for experiment tracking

---
+++ experiments.py
@@ -0,0 +1,61 @@
+# first party
+import os
+
+# third party
+import pandas as pd
+
+
+def build_structure_comparison_data(name: str) -> pd.DataFrame:
+    """
+    The goal of this function is to gather results from different
+    problems by name for comparison.
+
+    NOTE: At the time of function creation 10/82022 the methods are:
+    1. cnn-lbfgs (Google)
+    2. cnn-pygranso (UMN)
+    3. cnn-adam (UMN)
+    """
+    # default prefix list
+    experiment_prefix_list = ["google", "pygranso"]
+
+    # Set the path
+    path = "./results"
+
+    # Check that the files exist
+    for experiment in experiment_prefix_list:
+        filename = f"{experiment}_{name}.csv"
+        if not os.path.exists(path, filename):
+            raise ValueError(
+                "Need to gather results for " f"experiment {experiment}_{name}"
+            )
+
+    # Load results from google
+    google_filename = f"google_{name}.csv"
+    google_results = pd.read_csv(os.path.join(path, "results", google_filename))  # noqa
+
+    # Set the index to the steps
+    # First check that step is a column
+    if "step" not in google_results.columns:
+        raise ValueError("Google - expected column named step!")
+
+    # Set the index
+    google_results = google_results.set_index("step")
+
+    # Load the pygranso files
+    pygranso_filename = f"pygranso_{name}.csv"
+    pygranso_results = pd.read_csv(
+        os.path.join(path, "results", pygranso_filename)
+    )  # noqa
+
+    # Similarly set the index to step
+    if "step" not in pygranso_results.columns:
+        raise ValueError("Pygranso - expected column named step!")
+
+    # Set the index
+    pygranso_results = pygranso_results.set_index("step")
+
+    # Merge the data frames together
+    # If one has less iterations just forward fill the results
+    results = pd.merge(pygranso_results, google_results, on="step", how="left").ffill()  # noqa
+
+    return results
