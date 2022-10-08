b8d15e6c1e537a7ac5e4e65d936dd16a07a043a5
Ryan <ryan@tesorio.com>
2022-10-08 14:38:31 -0700

    checked building plots locally - pushing changes

--- experiments.py
+++ experiments.py
@@ -2,6 +2,7 @@
 import os
 
 # third party
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -23,15 +24,15 @@ def build_structure_comparison_data(name: str) -> pd.DataFrame:
 
     # Check that the files exist
     for experiment in experiment_prefix_list:
-        filename = f"{experiment}_{name}.csv"
-        if not os.path.exists(path, filename):
+        filename = f"{experiment}_{name}_results.csv"
+        if not os.path.exists(os.path.join(path, filename)):
             raise ValueError(
                 "Need to gather results for " f"experiment {experiment}_{name}"
             )
 
     # Load results from google
-    google_filename = f"google_{name}.csv"
-    google_results = pd.read_csv(os.path.join(path, "results", google_filename))  # noqa
+    google_filename = f"google_{name}_results.csv"
+    google_results = pd.read_csv(os.path.join(path, google_filename))  # noqa
 
     # Set the index to the steps
     # First check that step is a column
@@ -42,9 +43,9 @@ def build_structure_comparison_data(name: str) -> pd.DataFrame:
     google_results = google_results.set_index("step")
 
     # Load the pygranso files
-    pygranso_filename = f"pygranso_{name}.csv"
+    pygranso_filename = f"pygranso_{name}_results.csv"
     pygranso_results = pd.read_csv(
-        os.path.join(path, "results", pygranso_filename)
+        os.path.join(path, pygranso_filename)
     )  # noqa
 
     # Similarly set the index to step
@@ -56,6 +57,36 @@ def build_structure_comparison_data(name: str) -> pd.DataFrame:
 
     # Merge the data frames together
     # If one has less iterations just forward fill the results
-    results = pd.merge(pygranso_results, google_results, on="step", how="left").ffill()  # noqa
+    results = pd.merge(
+        pygranso_results, google_results, on="step", how="left"
+    ).ffill()  # noqa
 
     return results
+
+
+def save_loss_plots(name: str) -> None:
+    """
+    Function to gather the results of the data and create plots to
+    save for our papers regarding structural optimization
+    """
+    path = "./images"
+    results = build_structure_comparison_data(name=name)
+
+    # Create the figure & axis
+    fig, ax = plt.subplots(1, 1)
+
+    # plot the results
+    results.plot(lw=2, ax=ax)
+
+    # Set attributes of the plot
+    title = name.lower().split("_")
+    title = list(map(lambda x: x.capitalize(), title))
+    title = " ".join(title)
+    title = f"{title}: Method Comparisons"
+    ax.set_title(title)
+    ax.set_xlabel("step")
+    ax.set_ylabel("Compliance (loss function)")
+    ax.grid()
+
+    # Save the figure
+    plt.savefig(os.path.join(path, f"{name}_fig.png"))
