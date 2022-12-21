c7f586689c0b74174d326e6be4aa6a7679ecb245
Ryan <ryan@tesorio.com>
2022-12-21 08:16:58 -0800

    fixing bug

--- tasks.py
+++ tasks.py
@@ -110,7 +110,7 @@ def structural_optimization_task(problem_name, num_trials, maxit, resizes):
     )
     print("Finished training")
 
-    print(google_trails[:3])
+    print(google_trials[:3])
 
     # Google best trial
     google_best_trial = sorted(google_trials)[0]
