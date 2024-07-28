4efb3fd9faf2c0bdfbe5ba55785392fdda670533
Ryan Devera <rydevera@vpn-10-100-0-57.d.umn.edu>
2024-07-28 09:50:12 -0700

    adding the trajectories plotting to code

--- experiments.py
+++ experiments.py
@@ -10,7 +10,11 @@ import pandas as pd
 
 
 def build_optimization_trajectories(
-    path: str, problem_name: str, experiment_id: str, title_mapping: Dict
+    path: str,
+    problem_name: str,
+    experiment_id: str,
+    title_mapping: Dict,
+    font_size: int = 14,
 ) -> None:
     """
     Function that is used to build the optimization trajectories of
@@ -42,7 +46,7 @@ def build_optimization_trajectories(
 
     # The last element for each of the data sources is the dictionary of
     # training trajectories
-    # get the pygranso loss
+    # NTO-PCO Loss
     pygranso_data_trajectories = pygranso_data[-1]
     pygranso_loss = pd.Series(pygranso_data_trajectories['loss'].flatten())
 
@@ -50,87 +54,86 @@ def build_optimization_trajectories(
     # convergence
     print(f'Our method converged in {len(pygranso_loss.dropna())} iterations')
 
-    # Get the google loss
+    # Google-DIP Loss
     google_data_trajectories = google_data['google-cnn'][-1]
     google_loss = pd.Series(google_data_trajectories['loss'].values)
 
-    # Also need to get the MMA loss
+    # MMA Loss
     mma_data_trajectories = google_data['mma'][-1]
     mma_loss = pd.Series(mma_data_trajectories['loss'].values)
 
-    # Get the tounn loss
+    # TOuNN Loss
     tounn_data_trajectories = tounn_data[-1]
     tounn_loss = pd.Series(tounn_data_trajectories['loss'])
 
     # Plot the losses
-    # Compliance and iterations can be large so I am putting this plot
-    # on a log scale
+    # Compliance and iterations can be large so we are plotting with a log scale
     pygranso_loss.plot(color='blue', logx=True, logy=True, label='NTO-PCO', ax=ax1)
     google_loss.plot(color='red', label='Google-DIP', ax=ax1)
     mma_loss.plot(color='gold', label='MMA', ax=ax1)
     tounn_loss.plot(color='limegreen', label='TOuNN', ax=ax1)
 
-    # Add a grid and a title to the output
+    # Plot configs
     ax1.grid()
+    ax1.set_title('Log (Compliance)', fontsize=font_size)
 
-    # was weight='bold'
-    ax1.set_title('Log (Compliance)', fontsize=14)
-
-    # Get pygranso binary constraint
+    # NTO-PCO Binary Constraint
     pygranso_bc = np.abs(
         pd.Series(pygranso_data_trajectories['binary_constraint'].flatten())
     )
 
-    # Get the google and mma binary constraint
+    # Google-DIP and MMA Binary Constraint
     google_bc = np.abs(pd.Series(google_data_trajectories['binary_constraint']))
     mma_bc = np.abs(pd.Series(mma_data_trajectories['binary_constraint']))
 
-    # tounn binary constraint
+    # TOuNN Binary Constraint
     tounn_data_trajectories = tounn_data[-1]
     tounn_bc = np.abs(pd.Series(tounn_data_trajectories['binary_constraint']))
 
-    # Plot the binary constraints
+    # Plot Binary Constraints
     pygranso_bc.plot(color='blue', logx=True, label='NTO-PCO', ax=ax2)
     google_bc.plot(color='red', label='Google-DIP', ax=ax2)
     mma_bc.plot(color='gold', label='MMA', ax=ax2)
     tounn_bc.plot(color='limegreen', label='TOuNN', ax=ax2)
 
+    # Plot Configs
     ax2.grid()
-    ax2.set_title('Binary Constraint', fontsize=14)
-    ax2.set_xlabel('Log (Iterations)', fontsize=14)
+    ax2.set_title('Binary Constraint', fontsize=font_size)
+    ax2.set_xlabel('Log (Iterations)', fontsize=font_size)
 
     # To emphasize the goal of the paper - lets add a goal line
     # for the constraints
     ax2.text(0.75, 0.0, 'Goal', ha='left', va='bottom', color='gray')
     ax2.axhline(0.0, color='gray')
 
-    # Get the pygranso volume constraint
+    # NTO-PCO Volume Constraint
     pygranso_vc = np.abs(
         pd.Series(pygranso_data_trajectories['volume_constraint'].flatten())
     )
 
-    # Get the google and mma volume constraint
+    # Google-DIP and MMA Volume Constraint
     google_vc = np.abs(pd.Series(google_data_trajectories['volume_constraint']))
     mma_vc = np.abs(pd.Series(mma_data_trajectories['volume_constraint']))
 
-    # Get the tounn volume constraint
+    # TOuNN Volume Constraint
     tounn_vc = np.abs(pd.Series(tounn_data_trajectories['volume_constraint']))
 
-    # Plot the volume constraints
+    # Plot Volume Constraints
     pygranso_vc.plot(color='blue', logx=True, label='NTO-PCO', ax=ax3)
     google_vc.plot(color='red', label='Google-DIP', ax=ax3)
     mma_vc.plot(color='gold', label='MMA', ax=ax3)
     tounn_vc.plot(color='limegreen', label='TOuNN', ax=ax3)
 
+    # Plot configs
     ax3.grid()
-    ax3.set_title('Volume Constraint', fontsize=14)
+    ax3.set_title('Volume Constraint', fontsize=font_size)
 
     # Similar to the binary constraint I am also adding a goal for the
     # volume constraint
     ax3.text(0.75, 0.0, 'Goal', ha='left', va='bottom', color='gray')
     ax3.axhline(0.0, color='gray')
 
-    # Single legend for all plots outside of the plot
+    # Single legend defined at outside the plot
     handles, labels = ax3.get_legend_handles_labels()
     fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, -0.15), ncol=4)
 
